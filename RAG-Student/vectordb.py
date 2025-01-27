e3d8d178cef6d33e20e8ee17c03e55206892307b
henryhoangduong <henryhoangduong@gmail.com>
2025-01-27 15:09:58 +0700

    Rag-student

---
+++ RAG-Student/vectordb.py
@@ -0,0 +1,12 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from docx import Document
+from langchain_core.documents import Document as LC_Document
+from pypdf import PdfReader
+import torch
+import os
+import yaml
+
+
